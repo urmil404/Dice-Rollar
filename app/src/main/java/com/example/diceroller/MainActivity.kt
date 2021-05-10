f077ff78e1795ae9970d00aaff11d395ed53d6e1
urmil89 <urmil8989@gmail.com>
2021-05-10 13:47:56 +0530

    adding animation

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,27 +1,30 @@
 package com.example.diceroller
 
 import android.animation.Animator
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        dice.run {
+            dice.addAnimatorListener(object : Animator.AnimatorListener {
+                override fun onAnimationStart(animation: Animator) {}
+                override fun onAnimationEnd(animation: Animator) {
+                    hideDice()
+                    val random = (1..6).random()
+                    textView.text = random.toString()
+                }
+                override fun onAnimationCancel(animation: Animator) {}
 
-        val btnshow = findViewById(R.id.buttonroll) as Button
-
-        btnshow.setOnClickListener {
-            val randomNumber = (1..6).random()
-            val diceDigit: TextView = findViewById(R.id.textView)
-            diceDigit.text = randomNumber.toString()
+                override fun onAnimationRepeat(animation: Animator) {}
+            })
         }
-
-
     }
 
     private fun showDice() {
@@ -31,7 +34,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun hideDice() {
-
+        dice.visibility = View.GONE
+        textView.visibility = View.VISIBLE
+        buttonroll.isEnabled = true
     }
 
     fun rollDiceClicked(view: View) {
